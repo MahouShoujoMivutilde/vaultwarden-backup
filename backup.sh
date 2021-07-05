2f113d11b64c30b0f034b01f2e6a8d5aa0ed2f1e
MahouShoujoMivutilde <bbeedsi4@anonaddy.me>
2021-07-05 18:04:22 +0300

    Tailored it to my taste:
    Simplified user-facing variables;
    Age instead of gpg;
    `rclone sync archives/ ...` instead of `copy file.tar.xz ...`;
    7z instead of tar xz.

--- backup.sh
+++ backup.sh
@@ -2,22 +2,16 @@
 
 set -ex
 
-# Use the value of the corresponding environment variable, or the
-# default if none exists.
-: ${VAULTWARDEN_ROOT:="$(realpath "${0%/*}"/..)"}
-: ${SQLITE3:="/usr/bin/sqlite3"}
-: ${RCLONE:="/usr/local/bin/rclone"}
+if [ -f "$1" ]; then
+    source "$1"
+else
+    echo First argument must be your backup.conf
+    exit 1
+fi
 
-DATA_DIR="data"
 BACKUP_ROOT="${VAULTWARDEN_ROOT}/backup"
-BACKUP_DIR_NAME="vaultwarden-$(date '+%Y%m%d-%H%M')"
 BACKUP_DIR_PATH="${BACKUP_ROOT}/${BACKUP_DIR_NAME}"
-BACKUP_FILE_DIR="archives"
-BACKUP_FILE_NAME="${BACKUP_DIR_NAME}.tar.xz"
-BACKUP_FILE_PATH="${BACKUP_ROOT}/${BACKUP_FILE_DIR}/${BACKUP_FILE_NAME}"
-DB_FILE="db.sqlite3"
-
-source "${BACKUP_ROOT}"/backup.conf
+BACKUP_FILE_PATH="${BACKUP_ROOT}/archives/${BACKUP_DIR_NAME}.7z"
 
 cd "${VAULTWARDEN_ROOT}"
 mkdir -p "${BACKUP_DIR_PATH}"
@@ -29,10 +23,10 @@ mkdir -p "${BACKUP_DIR_PATH}"
 # but to minimize the possibility of a failed backup, implement a retry mechanism here.
 max_tries=10
 tries=0
-until ${SQLITE3} "file:${DATA_DIR}/${DB_FILE}?mode=ro" ".backup '${BACKUP_DIR_PATH}/${DB_FILE}'"; do
+until sqlite3 "file:${DATA_DIR}/${DB_FILE}?mode=ro" ".backup '${BACKUP_DIR_PATH}/${DB_FILE}'"; do
     if (( ++tries >= max_tries )); then
-	echo "Aborting after ${max_tries} failed backup attempts..."
-	exit 1
+        echo "Aborting after ${max_tries} failed backup attempts..."
+        exit 1
     fi
     echo "Backup failed. Retry #${tries}..."
     rm -f "${BACKUP_DIR_PATH}/${DB_FILE}"
@@ -46,25 +40,24 @@ for f in attachments config.json rsa_key.der rsa_key.pem rsa_key.pub.der sends; 
     fi
 done
 cp -a "${backup_files[@]}" "${BACKUP_DIR_PATH}"
-tar -cJf "${BACKUP_FILE_PATH}" -C "${BACKUP_ROOT}" "${BACKUP_DIR_NAME}"
-rm -rf "${BACKUP_DIR_PATH}"
-md5sum "${BACKUP_FILE_PATH}"
-sha1sum "${BACKUP_FILE_PATH}"
+7z a "$BACKUP_FILE_PATH" "$BACKUP_DIR_PATH"
 
-if [[ -n ${GPG_PASSPHRASE} ]]; then
-    # https://gnupg.org/documentation/manuals/gnupg/GPG-Esoteric-Options.html
-    # Note: Add `--pinentry-mode loopback` if using GnuPG 2.1.
-    printf '%s' "${GPG_PASSPHRASE}" |
-    gpg -c --cipher-algo "${GPG_CIPHER_ALGO}" --batch --passphrase-fd 0 "${BACKUP_FILE_PATH}"
-    BACKUP_FILE_NAME+=".gpg"
-    BACKUP_FILE_PATH+=".gpg"
-    md5sum "${BACKUP_FILE_PATH}"
-    sha1sum "${BACKUP_FILE_PATH}"
+echo "$BACKUP_DIR_PATH"
+rm -rf "$BACKUP_DIR_PATH"
+
+if [[ -n ${AGE_PUB} ]]; then
+    age -r "$AGE_PUB" -o "$BACKUP_FILE_PATH.age" "$BACKUP_FILE_PATH"
+    rm -rf "$BACKUP_FILE_PATH"
+    BACKUP_FILE_PATH+=".age"
 fi
 
 # Attempt uploading to all remotes, even if some fail.
 set +e
 
 for dest in "${RCLONE_DESTS[@]}"; do
-    ${RCLONE} -vv --no-check-dest copy "${BACKUP_FILE_PATH}" "${dest}"
+    rclone -vv sync "$(dirname "$BACKUP_FILE_PATH")" "${dest}"
 done
+
+# Purge old backups
+find "$(dirname "$BACKUP_FILE_PATH")" -name 'vaultwarden-*.7z' -mtime +60 -delete
+find "$(dirname "$BACKUP_FILE_PATH")" -name 'vaultwarden-*.age' -mtime +60 -delete
